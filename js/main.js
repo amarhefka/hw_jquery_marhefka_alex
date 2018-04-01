ec113f4e1d82a41e6d76201fb64aaed7efb545d7
Alex <alex.marhefka@gmail.com>
2018-03-31 21:56:39 -0400

    Stylize paragraphs with jQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-
+    var paragraphs = $('p');
+    $(paragraphs).css({
+        'border': '0.1em dotted #1e830d',
+        'color': '#1e830d',
+        'font-size': '1.25em',
+        'padding': '0.5em'
+    });
 });
